a2a0b19487c9099691c0f7651085af5dff04385e
Vera@versus <veraatversus@gmail.com>
2018-03-05 17:26:57 +0100

    com 1

---
+++ Plugins/Log/LogUserControl.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace HZBot
+{
+    /// <summary>
+    /// Interaktionslogik für LogUserControl.xaml
+    /// </summary>
+    public partial class LogUserControl : HzUserControl
+    {
+        #region Constructors
+
+        public LogUserControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+    }
+}
