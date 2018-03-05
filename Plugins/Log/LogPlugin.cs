486dc5afddcc00226fbcbf1c2fa754ca9d3f5d90
Vera@versus <veraatversus@gmail.com>
2018-03-05 22:12:35 +0100

    com 10

---
+++ Plugins/Log/LogPlugin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+
+namespace HZBot
+{
+    public class LogPlugin : HzPluginBase
+    {
+        #region Constructors
+
+        public LogPlugin(HzAccount account) : base(account)
+        {
+            Context = System.Threading.SynchronizationContext.Current;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public ObservableCollection<string> LogList { get; } = new ObservableCollection<string>();
+
+        public string GetLogsAsString
+        {
+            get
+            {
+                return String.Join("\n", LogList);
+            }
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        public void Add(string text)
+        {
+            Context.Send(o => LogList.Add(text), null);
+            //LogList.Add(text);
+            RaisePropertyChanged(nameof(GetLogsAsString));
+            // RaisePropertyChanged(nameof(LogList));
+        }
+
+        #endregion Methods
+
+        #region Fields
+
+        private SynchronizationContext Context;
+
+        #endregion Fields
+    }
+}
