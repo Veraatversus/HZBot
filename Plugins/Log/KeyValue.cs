e75bbab7eb5ddc97d2aec86a7615515a55d7986c
Vera@versus <veraatversus@gmail.com>
2018-03-11 20:32:37 +0100

    test

---
+++ Plugins/Log/KeyValue.cs
@@ -0,0 +1,12 @@
+﻿
+namespace HZBot
+{
+    public class KeyValue : ViewModelBase
+    {
+        private string _key;
+        private int _value;
+
+        public string Key { get => _key; set { _key = value; RaisePropertyChanged(); } }
+        public int Value { get => _value; set { _value = value; RaisePropertyChanged(); } }
+    }
+}
