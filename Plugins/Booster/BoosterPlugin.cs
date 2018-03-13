f8012f10804a7765df6b824b5af045dc0d63d89d
zotan <Zotan@outlook.de>
2018-03-13 19:30:49 +0100

    Booster2

--- Plugins/Booster/BoosterPlugin.cs
+++ Plugins/Booster/BoosterPlugin.cs
@@ -16,8 +16,18 @@ namespace HZBot
             var MBTime = Account.Data.character.ts_active_quest_boost_expires - Account.Data.server_time;
             var MBType = Account.Data.character.active_quest_booster_id;
             MBTime = (MBTime / 60) / 60;
-            Account.Log.Add($"MissionsBooster ID: {MBType} verbleibend: {MBTime}");
 
+            var SBTime = Account.Data.character.ts_active_stats_boost_expires - Account.Data.server_time;
+            var SBType = Account.Data.character.active_stats_booster_id;
+            SBTime = (SBTime / 60) / 60;
+
+            var WBTime = Account.Data.character.ts_active_work_boost_expires - Account.Data.server_time;
+            var WBType = Account.Data.character.active_work_booster_id;
+            WBTime = (WBTime / 60) / 60;
+
+            Account.Log.Add($"MissionsBooster ID: {MBType} verbleibend: {MBTime}");
+            Account.Log.Add($"StatsBooster ID: {SBType} verbleibend: {SBTime}");
+            Account.Log.Add($"WorkBooster ID: {WBType} verbleibend: {WBTime}");
             return;
         }
     }
