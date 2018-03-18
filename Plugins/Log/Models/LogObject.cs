61e486f728b134c47241349be87db8add55d518b
Vera@versus <veraatversus@gmail.com>
2018-03-18 20:15:05 +0100

    oh

---
+++ Plugins/Log/Models/LogObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HZBot
+{
+    public class LogObject : ViewModelBase
+    {
+        #region Properties
+
+        public string Text { get; set; }
+        public DateTime Time { get; set; }
+
+        public object Tooltip
+        {
+            get => tooltip;
+            set { tooltip = value; RaisePropertyChanged(); }
+        }
+
+        public RequestState RequestState
+        {
+            get => requestState;
+            set { requestState = value; RaisePropertyChanged(); }
+        }
+
+        public override string ToString()
+        {
+            return $"{DateTime.Now.Hour:00}:{DateTime.Now.Minute:00}:{DateTime.Now.Second:00} - {Text}";
+        }
+        #endregion Properties
+
+        #region Fields
+
+        private RequestState requestState;
+        private object tooltip;
+
+        #endregion Fields
+    }
+}
---
+++ Plugins/Log/UI/LogUserControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace HZBot
+{
+    /// <summary>
+    /// Interaktionslogik für LogUserControl.xaml
+    /// </summary>
+    public partial class LogUserControl : HzUserControl
+    {
+        #region Constructors
+
+        public LogUserControl()
+        {
+            InitializeComponent();
+            var cb = new CommandBinding(ApplicationCommands.Copy, CopyCmdExecuted, CopyCmdCanExecute);
+
+            this.LogListBox.CommandBindings.Add(cb);
+        }
+
+        #endregion Constructors
+
+        void CopyCmdExecuted(object target, ExecutedRoutedEventArgs e)
+        {
+            var lb = e.OriginalSource as System.Windows.Controls.ListBox;
+            var copyContent = String.Empty;
+
+            foreach (var item in lb.SelectedItems.OfType<LogObject>())
+            {
+                copyContent += item.Text;
+                copyContent += Environment.NewLine;
+            }
+            Clipboard.SetText(copyContent);
+        }
+        void CopyCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            var lb = e.OriginalSource as System.Windows.Controls.ListBox;
+
+            if (lb.SelectedItems.Count > 0)
+                e.CanExecute = true;
+            else
+                e.CanExecute = false;
+        }
+    }
+}
